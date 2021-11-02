715d04de92f4cf62cecb56648d31453dc9563f1d
unknown <iangle@ewu.edu>
2021-11-01 17:06:16 -0700

    added a way to move the data from the global memory to the shared memory

--- kernel1.cu
+++ kernel1.cu
@@ -12,23 +12,45 @@
 __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width) 
 {
     extern __shared__ float s_data[];
+
     // TODO, implement this kernel below
+    unsigned int i = blockIdx.y * blockDim.y + threadIdx.y;
     
+    // global thread(data) column index
+    unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if( i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
+
+   // int colNum = j % 3; // 3 is the number of rows we are looking at each time
+    
+    int northValue = g_dataA[(i-1) * pitch +  j];
+    int middleValue = g_dataA[i * pitch + j];
+    int southValue = g_dataA[(i+1) * pitch +  j];
+
+    s_data[(i - 1) * pitch + j] = northValue;
+    s_data[i * pitch + j] = middleValue;
+    s_data[(i + 1) * pitch + j] = southValue;
+
+    if(threadIdx.x == blockDim.x-1 && i + 2 < width && ((i + 3) * pitch + j) < width)
+    {
+        s_data[(i) * pitch + j] = g_dataA[(i) * pitch +  j];
+        s_data[(i + 1) * pitch + j] = g_dataA[(i + 1) * pitch +  j];
+        s_data[(i + 2) * pitch + j] = g_dataA[(i + 2) * pitch +  j];
+
+        s_data[(i + 1) * pitch + j] = g_dataA[(i + 1) * pitch +  j];
+        s_data[(i + 2) * pitch + j] = g_dataA[(i + 2) * pitch +  j];
+        s_data[(i + 3) * pitch + j] = g_dataA[(i + 3) * pitch +  j];
+    }
+
+    _syncthreads();
+    //float jacobiValue = runJacobi(colNum, northValue, middleValue, southValue);
+
+    //s_data[0] += jacobiValue;
 }
 
-__device__ float runJacobi(int currRow, int currCol, float* g_dataA, float* g_dataB, int pitch)
+__device__ float runJacobi(int colNum, int northValue, int middleValue, int southValue)
 {
 
-      g_dataB[currRow * pitch + currCol] = (
-                              0.2f * g_dataA[currRow * pitch + currCol]             +   //itself
-                              0.1f * g_dataA[(currRow - 1) * pitch +  currCol   ]   +   //N
-                              0.1f * g_dataA[(currRow - 1) * pitch + (currCol + 1)] +   //NE
-                              0.1f * g_dataA[ currRow    * pitch + (currCol + 1)]   +   //E
-                              0.1f * g_dataA[(currRow + 1) * pitch + (currCol + 1)] +   //SE
-                              0.1f * g_dataA[(currRow + 1) * pitch +  currCol   ]   +   //S
-                              0.1f * g_dataA[(currRow + 1) * pitch + (currCol - 1)] +   //SW
-                              0.1f * g_dataA[ currRow    * pitch + (currCol - 1)]   +   //W
-                              0.1f * g_dataA[(currRow - 1) * pitch + (currCol - 1)]     //NW
-                           ) * 0.95f;
+
 }
 
