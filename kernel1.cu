0a7b7976cba37423e8c23e4796f68405c4f9de11
unknown <iangle@ewu.edu>
2021-11-03 15:40:00 -0700

    got rid of an if statment

--- kernel1.cu
+++ kernel1.cu
@@ -26,8 +26,8 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
     if(i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
 
 
-    if(threadIdx.y != 0)
-    {
+    //if(threadIdx.y != 0)
+    //{
         s_data[blockDim.y * threadIdx.x + threadIdx.y] = g_dataA[i * floatpitch + j]; //middle
         s_data[blockDim.y * (threadIdx.x + 1) + threadIdx.y] = g_dataA[(i + 1) * floatpitch + j]; //south
         s_data[blockDim.y * (threadIdx.x - 1) + threadIdx.y] = g_dataA[(i - 1) * floatpitch + j]; //north
@@ -47,7 +47,7 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
             s_data[blockDim.y * (threadIdx.x - 1) + (threadIdx.y - 1)] = g_dataA[ (i - 1) * floatpitch + (j - 1)]; //NW
             printf("1");
         }
-    }
+    //}
 
     __syncthreads();
 
