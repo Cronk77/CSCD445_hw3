f02424e4892a5ea05bbe40494516597a15462370
unknown <iangle@ewu.edu>
2021-11-03 13:36:42 -0700

    fixed a typo

--- kernel1.cu
+++ kernel1.cu
@@ -25,7 +25,6 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if( i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
 
-    if()
     s_data[blockDim.y * threadIdx.x + (threadIdx.y + 1)] = g_dataA[i * floatpitch + j]; //middle
     s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)] = g_dataA[(i + 1) * floatpitch + j]; //south
     s_data[blockDim.y * (threadIdx.x - 1) + (threadIdx.y + 1)] = g_dataA[(i - 1) * floatpitch + j]; //north
