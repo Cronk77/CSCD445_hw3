349dad9f278f80845f8dd457a42c83b7f68f9bda
unknown <iangle@ewu.edu>
2021-11-03 13:35:57 -0700

    fixed an issue with the blocks now being fully computed

--- kernel1.cu
+++ kernel1.cu
@@ -25,6 +25,7 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if( i >= width - 1|| j >= width - 1 || i < 1 || j < 1 ) return;
 
+    if()
     s_data[blockDim.y * threadIdx.x + (threadIdx.y + 1)] = g_dataA[i * floatpitch + j]; //middle
     s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)] = g_dataA[(i + 1) * floatpitch + j]; //south
     s_data[blockDim.y * (threadIdx.x - 1) + (threadIdx.y + 1)] = g_dataA[(i - 1) * floatpitch + j]; //north
@@ -48,20 +49,18 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
     float south = s_data[blockDim.y * (threadIdx.x + 2) + (threadIdx.y + 1)];
     float north = s_data[blockDim.y * threadIdx.x + (threadIdx.y + 1)];
 
-    int colNum = threadIdx.y % 2; // 3 is the number of columns per jacobi value and we view two of them at first so we only look at each one twice
+    int colNum = threadIdx.y % 3; // 3 is the number of columns per jacobi value
 
     float jacobiValue = 1.0;
 
     //if we are at the first value then we grab the previous value as well otherwise we add up all the values and multiply by .95 to get the jacobi value
     if(colNum == 0)
     {
-        float prevMiddle = s_data[blockDim.y * (threadIdx.x + 1) + threadIdx.y];
-        float prevSouth = s_data[blockDim.y * (threadIdx.x + 2) + threadIdx.y];
-        float prevNorth = s_data[blockDim.y * threadIdx.x + threadIdx.y];
+        s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)] = runJacobi(0, north, middle, south); 
 
-        s_data[blockDim.y * (threadIdx.x + 1) + threadIdx.y + 1] = runJacobi(1, north, middle, south); // isMiddle is 1 because we are at the middle number
-        s_data[blockDim.y * (threadIdx.x + 1) + threadIdx.y] = runJacobi(0, prevNorth, prevMiddle, prevSouth); // isMiddle is 0 because we are not at the middle numbe
-
+    }else if (colNum == 1)
+    {
+        s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)] = runJacobi(1, north, middle, south); 
     }else
     {
         jacobiValue = runJacobi(0, north, middle, south);
@@ -73,15 +72,16 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
     __syncthreads();
 
     //if we have a correct jacobiValue, store it at the middle location
-    if(colNum == 1)
+    if(colNum == 2)
     {
         s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)] = jacobiValue;
     }
 
     __syncthreads();
 
-    //move data to output array -- might be padded so this might not work --
-    g_dataB[i * floatpitch + j] = s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)];
+    //move data to output array
+    if(blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1) < blockDim.y)
+        g_dataB[i * floatpitch + j] = s_data[blockDim.y * (threadIdx.x + 1) + (threadIdx.y + 1)];
 }
 
 __device__ float runJacobi(int isMiddle, float northValue, float middleValue, float southValue)
