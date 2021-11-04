1edc1a86cab6ef419348f8d005961e7cc8f6e211
unknown <iangle@ewu.edu>
2021-11-03 21:25:55 -0700

    testing again

--- kernel1.cu
+++ kernel1.cu
@@ -29,10 +29,10 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if(threadIdx.x == 0)
     {
-        s_data[threadIdx.x + blockDim.x] = g_dataA[ i * floatpitch + (j - 2)]; //W
-        s_data[threadIdx.x + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 2)]; //SW
-        s_data[threadIdx.x] = g_dataA[ (i - 1) * floatpitch + (j - 2)]; //NW
-    }else if(threadIdx.x == 1 && i == 1)
+        s_data[threadIdx.x + blockDim.x] = g_dataA[ i * floatpitch + (j - 1)]; //W
+        s_data[threadIdx.x + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 1)]; //SW
+        s_data[threadIdx.x] = g_dataA[ (i - 1) * floatpitch + (j - 1)]; //NW
+    }else if(threadIdx.x == 1)
     {
         s_data[threadIdx.x + blockDim.x] = g_dataA[i * floatpitch + j]; //middle
         s_data[threadIdx.x + (2 * blockDim.x)] = g_dataA[(i + 1) * floatpitch + j]; //south
