9268f6fba53072fd3c755192d034c5cd45a25b98
unknown <iangle@ewu.edu>
2021-11-03 20:16:06 -0700

    fixed a minor ( error

--- kernel1.cu
+++ kernel1.cu
@@ -29,8 +29,8 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if(threadIdx.x == 0)
     {
-        s_data[(threadIdx.x + blockDim.x] = g_dataA[ i * floatpitch + (j - 1)]; //W
-        s_data[(threadIdx.x + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 1)]; //SW
+        s_data[threadIdx.x + blockDim.x] = g_dataA[ i * floatpitch + (j - 1)]; //W
+        s_data[threadIdx.x + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 1)]; //SW
         s_data[threadIdx.x] = g_dataA[ (i - 1) * floatpitch + (j - 1)]; //NW
     }else
     {
