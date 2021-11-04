a8427053bfb5a7345f62f5e38ddbe49d56d5ecd8
unknown <iangle@ewu.edu>
2021-11-04 15:02:44 -0700

    testing

--- kernel1.cu
+++ kernel1.cu
@@ -25,9 +25,9 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if(i >= width - 1 || j >= width - 1 || i < 1 || j < 1 ) return;
 
-    if( j == 1 && threadIdx.x == 1)
+    if(threadIdx.x == 1)
     {
-        printf("this ran!");
+        printf("%d\n", j);
         s_data[(threadIdx.x - 1) + blockDim.x] = g_dataA[ i * floatpitch + (j - 2)]; //W
         s_data[(threadIdx.x - 1) + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 2)]; //SW
         s_data[(threadIdx.x - 1)] = g_dataA[ (i - 1) * floatpitch + (j - 2)]; //NW
