afbdfa704af9bb294803156559aa682ed32f4c1d
unknown <iangle@ewu.edu>
2021-11-03 22:07:53 -0700

    changed the bounds

--- kernel1.cu
+++ kernel1.cu
@@ -30,7 +30,7 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
         s_data[threadIdx.x + blockDim.x] = g_dataA[ i * floatpitch + (j - 1)]; //W
         s_data[threadIdx.x + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j - 1)]; //SW
         s_data[threadIdx.x] = g_dataA[ (i - 1) * floatpitch + (j - 1)]; //NW
-    }else if(threadIdx.x == blockDim.x - 2)
+    }else if(threadIdx.x == blockDim.x - 1)
     {
         s_data[(threadIdx.x + 1) + blockDim.x] = g_dataA[ i * floatpitch + (j + 1)]; //E
         s_data[(threadIdx.x + 1) + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j + 1)]; //SE
