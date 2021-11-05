57ecdf77f7716cd02f9495913b5c9460167ca4b8
unknown <iangle@ewu.edu>
2021-11-04 22:04:12 -0700

    testimng

--- kernel1.cu
+++ kernel1.cu
@@ -65,20 +65,20 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
     __syncthreads();
 
     //shift threadIdx.x by one because the entire matrix is shifted
-    unsigned int currPosition = threadIdx.x + 1;
+    unsigned int currPosition = threadIdx.x;
 
     //if(currPosition > blockDim.x - 1) return;
 
     g_dataB[i * floatpitch + j] = (
-                            0.2f * s_data[currPosition + blockDim.x]             +       //itself
-                            0.1f * s_data[currPosition]                          +       //N
-                            0.1f * s_data[currPosition + 1]                      +       //NE
-                            0.1f * s_data[(currPosition + 1) + blockDim.x]       +       //E
-                            0.1f * s_data[(currPosition + 1) + (2 * blockDim.x)] +       //SE
-                            0.1f * s_data[currPosition + (2 * blockDim.x)]       +       //S
-                            0.1f * s_data[(currPosition - 1) + (2 * blockDim.x)] +       //SW
-                            0.1f * s_data[(currPosition - 1) + blockDim.x]       +       //W
-                            0.1f * s_data[currPosition - 1]                              //NW
+                            0.2f * s_data[currPosition + 1 + blockDim.x]             +       //itself
+                            0.1f * s_data[currPosition + 1]                          +       //N
+                            0.1f * s_data[currPosition + 2]                      +       //NE
+                            0.1f * s_data[(currPosition + 2) + blockDim.x]       +       //E
+                            0.1f * s_data[(currPosition + 2) + (2 * blockDim.x)] +       //SE
+                            0.1f * s_data[currPosition + 1 + (2 * blockDim.x)]       +       //S
+                            0.1f * s_data[(currPosition) + (2 * blockDim.x)] +       //SW
+                            0.1f * s_data[(currPosition) + blockDim.x]       +       //W
+                            0.1f * s_data[currPosition]                              //NW
                         ) * 0.95f;
 }
 
