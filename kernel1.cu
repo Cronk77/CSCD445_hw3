a0606e6d0c3447ba79c11431e3a36b6f79f37283
unknown <iangle@ewu.edu>
2021-11-04 20:47:27 -0700

    test

--- kernel1.cu
+++ kernel1.cu
@@ -25,7 +25,7 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
 
     if(i >= width - 1 || j >= width - 1 || i < 1 || j < 1 ) return;
 
-    if(threadIdx.x >= blockDim.x - 2)
+    if(threadIdx.x >= blockDim.x - 2 || (threadIdx.x == 0 && blockIdx.x != 0))
     {}else{
         if(threadIdx.x == 0)
         {
