d2f53d681b4c0b6eeba1eb0946a839164a6d8545
unknown <iangle@ewu.edu>
2021-11-04 19:18:23 -0700

    test

--- kernel1.cu
+++ kernel1.cu
@@ -61,8 +61,8 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
         s_data[(threadIdx.x + 2) + (2 * blockDim.x)] = g_dataA[ (i + 1) * floatpitch + (j + 1)]; //SE
         s_data[(threadIdx.x + 2)] = g_dataA[ (i - 1) * floatpitch + (j + 1)]; //NE
 
-        printf("first: %f %f %f\n\n", s_data[threadIdx.x], s_data[threadIdx.x + blockDim.x], s_data[threadIdx.x + (2 * blockDim.x)]);
-        printf("second: %f %f %f\n\n", s_data[threadIdx.x + 2], s_data[threadIdx.x + blockDim.x + 2], s_data[threadIdx.x + 2 + (2 * blockDim.x)]);
+        //printf("first: %f %f %f\n\n", s_data[threadIdx.x], s_data[threadIdx.x + blockDim.x], s_data[threadIdx.x + (2 * blockDim.x)]);
+        //printf("second: %f %f %f\n\n", s_data[threadIdx.x + 2], s_data[threadIdx.x + blockDim.x + 2], s_data[threadIdx.x + 2 + (2 * blockDim.x)]);
     }else
     {
         //grab current value in the global memory and store at next value in shared memory because it should be at 1
