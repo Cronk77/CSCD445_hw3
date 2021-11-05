7ffbd92a24b26f278bb3ef9c83a5209290c69483
unknown <iangle@ewu.edu>
2021-11-04 18:25:18 -0700

    test

--- kernel1.cu
+++ kernel1.cu
@@ -41,7 +41,7 @@ __global__ void k1( float* g_dataA, float* g_dataB, int floatpitch, int width)
         //printf("first: %f %f %f\n\n", s_data[threadIdx.x + 1], s_data[threadIdx.x + 1 + blockDim.x], s_data[threadIdx.x + 1 + (2 * blockDim.x)]);
         //printf("second: %f %f %f\n\n", s_data[threadIdx.x + 2], s_data[threadIdx.x + blockDim.x + 2], s_data[threadIdx.x + 2 + (2 * blockDim.x)]);
 
-    }else if(j == width - 2 || threadIdx.x == blockDim.x - 2)
+    }else if(j == width - 2 || threadIdx.x == blockDim.x)
     {
         //grab current value in the global memory and store at next value in shared memory because it should be at 1
         s_data[(threadIdx.x + 1) + blockDim.x] = g_dataA[ i * floatpitch + j]; //middle
